ff72bf213ca10e9f3b40415f29c68c301f6d1c35
Tung Le <tung.le0319@gmail.com>
2023-12-07 15:55:39 -0700

    Initial commit

---
+++ configuration/modules.js
@@ -0,0 +1,14 @@
+// https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-modules
+export default {
+  modules: [
+    '@nuxtjs/style-resources',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/apollo',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/gtm',
+    '@nuxtjs/i18n',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+}
